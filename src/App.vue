3d15cc28ef84c19b09edd32f914bdc9b791b644a
imxiaochen0v0 <im-xiaochen@outlook.com>
2024-01-11 02:00:33 +0800

    pinia持久化和统一管理

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
-<script setup></script>
+<script setup>
+import { useUserStore } from '@/stores/index'
+
+const userStore = useUserStore()
+</script>
 
 <template>
   <div>
     <RouterView></RouterView>
+
+    <p>{{ userStore.token }}</p>
+    <el-button @click="userStore.setToken('imxiaochen')">登录</el-button>
+    <el-button @click="userStore.setToken('')">注册</el-button>
   </div>
 </template>
 
