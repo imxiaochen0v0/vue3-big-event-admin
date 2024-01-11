443cac9aaebd9671b7e8eee07de094829becc2b2
imxiaochen0v0 <im-xiaochen@outlook.com>
2024-01-11 18:20:43 +0800

    layout架子和登录访问拦截

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores/index'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -35,6 +36,14 @@ const router = createRouter({
       ]
     }
   ]
+})
+
+router.beforeEach((to) => {
+  // 判断是否登录
+  const useStore = useUserStore()
+  if (!useStore.token && to.path !== '/login') {
+    return '/login'
+  }
 })
 
 export default router
